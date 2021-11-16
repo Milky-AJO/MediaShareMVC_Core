08780ac33006c3ed5311b02de68fe6c5671086cc
Andréas J. Otto <milkyotto@gmail.com>
2021-11-16 19:44:08 +0200

    Database Connection

---
+++ MediaShareMVC_Core/Areas/Identity/Data/WebAppUser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace MediaShareMVC_Core.Areas.Identity.Data
+{
+    // Add profile data for application users by adding properties to the WebAppUser class
+    public class WebAppUser : IdentityUser
+    {
+        [PersonalData]
+        [Column(TypeName = "nvarchar(100)")]
+        public string FName { get; set; }
+
+        [PersonalData]
+        [Column(TypeName = "nvarchar(100)")]
+        public string LName { get; set; }
+    }
+}
---
+++ MediaShareMVC_Core/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using MediaShareMVC_Core.Areas.Identity.Data;
+using MediaShareMVC_Core.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(MediaShareMVC_Core.Areas.Identity.IdentityHostingStartup))]
+namespace MediaShareMVC_Core.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<MediaShareDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("MediaShareDbContextConnection")));
+
+                services.AddDefaultIdentity<WebAppUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<MediaShareDbContext>();
+            });
+        }
+    }
+}
